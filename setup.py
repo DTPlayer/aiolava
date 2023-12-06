f1128955757f3be1e1d4d332f80b085c9dbc5fb8
reff06 <info@dnsprovider.tech>
2023-12-06 13:37:10 +0300

    update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
   name='aiolavapy',
-  version='0.1.0',
+  version='0.1.2',
   author='reff06',
   author_email='info@dnsprovider.tech',
   description='Async library for Lava API',
